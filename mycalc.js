e8b047f47baaad50d13f5db82ead0e40c8626060
Favour Christopher <kristyeva93@gmail.com>
2020-07-31 09:40:37 +0100

    Modify html, css and js files

--- mycalc.js
+++ mycalc.js
@@ -3,7 +3,7 @@ const bufferDisplay = document.querySelector('#buffer-display');
 let input = document.getElementById('input');
 let buffer = 0;
 let lastOperator;
-input.value = 0;
+input.innerHTML = '0';
 
 const handleClick = function(value) {
     if(isNaN(parseInt(value))){
@@ -15,91 +15,104 @@ const handleClick = function(value) {
 }
 
 const handleOperators = function(value){
-    if(input.value !== '0'){
         switch(value){
             case 'C':
-            input.value = 0;
+            bufferDisplay.innerHTML = "";
+            input.innerHTML = '0';
             buffer = 0;
-            bufferDisplay.innerHTML = '';
             break;
 
             case '←':
-            if(input.value.length === 1){
-                input.value = 0;
-            }
-            else{
-                let newInput = input.value.slice(0, input.value.length-1);
-                input.value = newInput;
+            if(input.innerHTML !== '0'){
+                if(input.innerHTML.length === 1){
+                    input.innerHTML = '0';
+                }
+                else{
+                    let newInput = input.innerHTML.slice(0, input.innerHTML.length-1);
+                    input.innerHTML = newInput;
+                }
             }
             break;
 
             case '÷':
-            lastOperator = '/';
-            performOperation(lastOperator);
-            input.value = 0;
+            if(input.innerHTML !== '0'){
+                lastOperator = '/';
+                performOperation(lastOperator);
+                input.innerHTML = '0';
+            }
             break;
 
             case '×':
-            lastOperator = '*';
-            performOperation(lastOperator);
-            input.value = 0;
+            if(input.innerHTML !== '0'){
+                lastOperator = '*';
+                performOperation(lastOperator);
+                input.innerHTML = '0';
+            }
             break;
 
             case '-':
-            lastOperator = '-';
-            performOperation(lastOperator);
-            input.value = 0;
+            if(input.innerHTML !== '0'){
+                lastOperator = '-';
+                performOperation(lastOperator);
+                input.innerHTML = '0';
+            }
             break;
 
             case '+':
-            lastOperator = '+';
-            performOperation(lastOperator);
-            input.value = 0;
+            if(input.innerHTML !== '0'){
+                lastOperator = '+';
+                performOperation(lastOperator);
+                input.innerHTML = '0';
+            }
             break;
 
             case '=':
-            lastOperator = '=';
-            buffer = eval(bufferDisplay.innerHTML+ input.value);
-            input.value = buffer;
-            bufferDisplay.innerHTML = '';
+            if(input.innerHTML !== '0'){
+                lastOperator = '=';
+                buffer = eval(bufferDisplay.innerHTML+ input.innerHTML);
+                input.innerHTML = buffer;
+                bufferDisplay.innerHTML = '';
+            }
             break;
 
             case '.':
-            if(!input.value.includes('.')){
-                input.value += value;
+            if(input.innerHTML !== '0'){
+                if(!input.innerHTML.includes('.')){
+                    input.innerHTML += value;
+                }
             }
             break;
         }
-    }
 }
 
 const handleNumbers = function(value){
-    if(lastOperator === '=' || input.value === '0'){
-        input.value = value;
+    if(lastOperator === '=' || input.innerHTML === '0'){
+        input.innerHTML = value;
+        lastOperator = '';
     }
     else{
-        input.value += value;
+        input.innerHTML += value;
     }
 }
 
 const performOperation = function(value){
     
         if(bufferDisplay.innerHTML === ""){
-            buffer = parseFloat(input.value);
+            buffer = parseFloat(input.innerHTML);
             bufferDisplay.innerHTML = buffer +' '+value;
         }
         else{
-            if(input.value === '0'){
+            if(input.innerHTML === '0'){
                 let bufferDisplayArray =bufferDisplay.innerHTML.split('');
                 bufferDisplayArray[bufferDisplayArray.length-1] = value;
                 bufferDisplay.innerHTML = bufferDisplayArray.join('');
             }
             else{
-                buffer = eval(bufferDisplay.innerHTML+ input.value);
+                buffer = eval(bufferDisplay.innerHTML+ input.innerHTML);
                 bufferDisplay.innerHTML = buffer +' '+value;
             }
         }
-        input.value = 0;
+        input.innerHTML = '0';
 }
 buttons.addEventListener('click', function(){
     handleClick(event.target.innerHTML);
